43afc9e1d225a8db9888eee9095abcd5d804f522
Mandamba <marcosmandamba@gmail.com>
2024-03-11 22:27:25 +0100

    changed grafbase directory

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,39 @@
+import { graph, config, scalar} from '@grafbase/sdk'
+
+
+const g = graph.Standalone()
+
+const User = g.type(
+  'User', {
+    name: scalar.string(),
+    email: scalar.email(), 
+    avatarUrl: scalar.url(),
+    description: scalar.string().optional(),
+    gitHubUrl: scalar.string().optional(),
+    linkedInUrl: scalar.url().optional(),
+  }
+);
+
+const Projects = g.type(
+  "Project",{
+    title: scalar.string(),
+    description: scalar.string(),
+    imageUrl: scalar.url(),
+    liveSiteUrl: scalar.url(),
+    githubUrl: scalar.url(),
+    category: scalar.string(),
+    createdBy: g.ref(User).list()
+  }
+);
+
+const grafbaseConfig = config({
+  graph: g,
+  auth: {
+    rules: (rules) => {
+      rules.public();
+    },
+  },
+});
+
+export default grafbaseConfig
+
